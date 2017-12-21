bad9ee6afc19ad14d3251dcd63515c498741522c
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-21 19:50:57 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,14 @@
+mport java.util.*;
+public class leap{
+      public static void main(String aa[]){
+            int a;
+            Scanner s=new Scanner(System.in);
+             a=s.nextInt();
+             if(a%4==0){
+                System.out.println("IT IS LEAP YEAR"); 
+             }
+             else{
+                 System.out.println("IT IS  NOT LEAP YEAR"); 
+             }
+    }
+}
