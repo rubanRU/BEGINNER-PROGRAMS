0ccde6b12344667f324d5a9ae5afe9c980d5e188
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-21 20:11:16 +0530

    Create len.java

---
+++ len.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class len{
+      public static void main(String aa[]){
+            int n;
+            Scanner s=new Scanner(System.in);
+            n=s.nextInt();
+            String j=String.valueOf(n);
+            int l=j.length();
+            System.out.println(l);
+            
+    }
+}
