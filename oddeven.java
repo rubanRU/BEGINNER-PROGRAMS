4f05736b2ecd0ae6401e7761dc0102136646041c
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-21 18:48:44 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Oddeve{
+      public static void main(String aa[]){
+            int n;
+            Scanner s=new Scanner(System.in);
+            n=s.nextInt();
+            if(n%2==0){
+                System.out.println("Even");
+            }
+         else{
+                System.out.println("Odd");
+           }
+             
+      }
+
+}
