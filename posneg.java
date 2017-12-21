3937323f825b50563e25c212c0e58ab808b60998
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-21 18:58:14 +0530

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class posneg{
+      public static void main(String aa[]){
+            int n;
+            Scanner s=new Scanner(System.in);
+            n=s.nextInt();
+            if(n>0){
+                System.out.println("Positive");
+            }
+          else if(n<0){
+               System.out.println("Negative");
+          }
+       else{
+                 System.out.println("Zero");
+       }
+           
+       
+           
+             
+      }
+
+}
