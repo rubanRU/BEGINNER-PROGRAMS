260fec557955cd396fbecf15fac70d447227005e
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-22 16:28:37 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+public class mul{
+      public static void main(String aa[]){
+           int n,p,q;
+           int f;
+           int sum=0;
+           Scanner s=new Scanner(System.in);
+           n = s.nextInt();
+           for(int i=n;i<=n*n;i+=n){
+               System.out.printf("%d ",i);
+           }
+           
+}
+}
