cb9495cee1490d13790aad7a40129060f79b8e4a
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-22 13:38:09 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+public class repeat{
+      public static void main(String aa[]){
+           int n,p;
+           int f=1;
+           Scanner s=new Scanner(System.in);
+           n=s.nextInt();
+           for(int i=1;i<=n;i++){
+               f=f*i;
+               
+           }
+System.out.printf("%d ",f);
+}
+}
