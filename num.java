b692c995ab5ee052c661dc12822fbcd293ea7d4f
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-23 20:19:15 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class num{
+      public static void main(String aa[]){
+       char d;
+       Scanner s=new Scanner(System.in);
+       d=s.next().charAt(0);
+       if(d>='0'&&d<='9'){
+           System.out.println("Yes");
+       }
+      else{
+           System.out.println("No");
+      }
+      
+      
+      
+}
+}
