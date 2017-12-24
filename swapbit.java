019a5d7f7a5fad896ab533a533324a45516c20f2
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-24 14:30:46 +0530

    Create swapbit.java

---
+++ swapbit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+public class swapbit{
+     public static void main(String aa[]){
+     int a;
+     int b;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+     a=a^b;
+     b=a^b;
+     a=a^b;
+     System.out.printf("%d %d",a,b);
+
+    }
+}
