35465a92c4f77676e0621ba2cd8e237c1d9ef3c9
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-24 10:48:55 +0530

    Create meg.java

---
+++ meg.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+public class meg{
+     public static void main(String aa[]){
+        int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+
+        }
+       Arrays.sort(a);
+       int y=n/2;
+       for(int i=0;i<n;i++){
+           if(i==y){
+               System.out.println(a[i]);
+           }
+       }
+
+      
+}
+}
