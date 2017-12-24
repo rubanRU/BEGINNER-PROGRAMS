47b1681403d4d47cdb63ae595119da9bca303c74
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-24 10:29:39 +0530

    Create tim.java

---
+++ tim.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+public class tim{
+     public static void main(String aa[]){
+        int h1;
+        int h2;
+        int m1;
+        int m2;
+        int dif,dif1,dif2;
+        Scanner s=new Scanner(System.in);
+        h1=s.nextInt();
+        h2=s.nextInt();
+        m1=s.nextInt();
+        m2=s.nextInt();
+        h1=h1*60+m1;
+        h2=h2*60+m2;
+        dif2=h2-h1;
+        if(dif2>60){
+            h1=dif2/60;
+            h2=dif2%60;
+            System.out.printf("%d %d",h1,h2);
+
+        }
+       else{
+            System.out.println(dif2);
+       }
+
+
+      
+}
+}
