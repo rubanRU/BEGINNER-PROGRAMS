e2c3a8431075cbd3ddc21818ad19cf1b4cc036ed
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-24 14:17:55 +0530

    Create numec.java

---
+++ numec.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class numec{
+     public static void main(String aa[]){
+      String a;
+      int count=0;
+      Scanner s=new Scanner(System.in);
+      a=s.nextLine();
+     for(int i=0;i<a.length();i++){
+        if(a.charAt(i)>='0'&&a.charAt(i)<='9'){
+             count++;
+        }
+     }
+     System.out.println(count);
+    }
+
+      
+}
