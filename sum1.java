ce822a21c3ca8079f98f6f5765792d62af553656
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 18:21:15 +0530

    Rename sum.java to sum1.java

---
+++ sum1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class sum{
+     public static void main(String aa[]){
+     int a,b,f;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+     f=a+b;
+     System.out.println(f);
+    }
+}
