73aa2614b14dfd59720d899cc484cc26a9973c72
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 21:15:28 +0530

    Create worc.java

---
+++ worc.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class worc{
+     public static void main(String aa[]){
+     String d;
+     int n=0;;
+     Scanner s=new Scanner(System.in);
+     d=s.nextLine();
+     String a[]=d.split("\\s+");
+     int l=a.length;
+     System.out.println(l);
+}
