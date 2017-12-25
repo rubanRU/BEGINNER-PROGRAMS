9e53e82344107667be4698eb1707c2ccb84c5b15
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 19:52:57 +0530

    Create mulo.java

---
+++ mulo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class mulo{
+     public static void main(String aa[]){
+     int n,m;
+     int sum=0;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     m=s.nextInt();
+     sum=n*m;
+     if(sum%2==0){
+         System.out.println("even");
+     }
+ else{
+         System.out.println("odd");
+ }
+
+}
+}
