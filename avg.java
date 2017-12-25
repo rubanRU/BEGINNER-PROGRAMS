2028f12d63ed9e9e9cf1979c363e30c76a61c8c0
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 18:19:01 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class avg{
+     public static void main(String aa[]){
+     int a,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+   for(int i=1;i<=a;i++){
+       f=f+i;
+   }
+     int g=f/a;
+     System.out.println(g);
+     
+
+    }
+}
