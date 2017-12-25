8db68bcf05216f51222616e3785715791f4317eb
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 18:06:57 +0530

    Rename len.java to len1.java

---
+++ len1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class len {
+     public static void main(String aa[]){
+     int a,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     String c=String.valueOf(a);
+     int l=c.length();
+     System.out.println(l);
+     
+
+    }
+}
