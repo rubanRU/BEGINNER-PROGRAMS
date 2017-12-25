8ecd81aa05484fa6762ea33da28380081ef98fc6
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 20:06:57 +0530

    Create s.java

---
+++ s.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+public class s {
+     public static void main(String aa[]){
+     int a;
+     int b;
+     int t;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+     t=a;
+     a=b;
+     b=t;
+     System.out.printf("%d %d",a,b);
+    }
+}
