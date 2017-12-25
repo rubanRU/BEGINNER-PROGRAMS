c83c8b639ca6088eab8f539736ae6b689b991513
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 20:12:00 +0530

    Create fib.java

---
+++ fib.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+public class fib{
+     public static void main(String aa[]){
+     int a=0,b=1;
+     int c;
+     int n;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+
+      for(int i=0;i<n;i++){
+          System.out.println(a);
+          c=a+b;
+          a=b;
+          b=c;
+      }
+     }
