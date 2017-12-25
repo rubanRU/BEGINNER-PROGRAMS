afdbd26a65c7849a778333107e1f7c950d9b8ed7
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 18:08:42 +0530

    Create nextn.java

---
+++ nextn.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class nextn {
+     public static void main(String aa[]){
+     int a,f=0;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+    a=a+1;
+    System.out.print(a);
+     
+
+    }
+}
