b8c3f4e380bd1c9edb964f1e8d5776163c134209
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 21:19:45 +0530

    Create odde.java

---
+++ odde.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class odde{
+     public static void main(String aa[]){
+     int a,b;
+     int n;
+     Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    n=a+b;
+    if(n%2==0){
+        System.out.println("even");
+    }
+ else{
+        System.out.println("odd");
+ }
+}
+    }
