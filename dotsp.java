e661efaf7417952abeb976b0a37fca35b53f5b05
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 21:24:31 +0530

    Create dotsp.java

---
+++ dotsp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class dotsp{
+     public static void main(String aa[]){
+     String a;
+     Scanner s=new Scanner(System.in);
+     a=s.nextLine();
+     String h[]=a.split("\\.");
+     for(int i=0;i<h.length;i++){
+         System.out.printf("%s ",h[i]);
+     }
+   
+}
+    }
+
