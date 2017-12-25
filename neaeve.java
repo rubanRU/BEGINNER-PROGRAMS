979e02d3190ef1e3399313c94e967bc7559837e1
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 19:49:58 +0530

    Create neaeve.java

---
+++ neaeve.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class neaeve{
+     public static void main(String aa[]){
+     int n;
+     int sum=0;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     if(n%2!=0){
+         System.out.print(n-1);
+     }
+
+}
+}
