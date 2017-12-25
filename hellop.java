8e23c3de7a922254359b66682cd615ad28545953
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-25 17:33:19 +0530

    Create hellop.java

---
+++ hellop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class hellop{
+     public static void main(String aa[]){
+     String a;
+     int k;
+     Scanner s=new Scanner(System.in);
+     k=s.nextInt();
+     a=s.next();
+     for(int i=1;i<=k;i++){
+         System.out.println(a);
+     }
+    
+     
+
+    }
+}
