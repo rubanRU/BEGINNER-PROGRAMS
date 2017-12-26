89c11cc8fc55f918dd9100aadf199e3881ff76b1
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 21:48:03 +0530

    Create kelv.java

---
+++ kelv.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class kelv{
+
+     public static void main(String aa[]){
+    int a,b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    double n=a+273.5;
+    System.out.println(n);
+
+           
+   }
+}
