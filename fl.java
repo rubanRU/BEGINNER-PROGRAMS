41d83f390998d25d5e5c2662e33c34460126b71f
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 19:32:10 +0530

    Create fl.java

---
+++ fl.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class fl{
+     public static void main(String aa[]){
+       Scanner s=new Scanner(System.in);
+       float a=s.nextFloat();
+       float b=s.nextFloat();
+       float c=a*b;
+       System.out.printf("%f",c);
+}
+    }
