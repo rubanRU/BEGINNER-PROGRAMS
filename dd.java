b13cca6f1285411c54ef6d29f6272e739f7eae1b
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 21:51:05 +0530

    Create dd.java

---
+++ dd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class dd{
+
+     public static void main(String aa[]){
+    int a,b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    for(int i=1;i<=a;i++){
+        if(b==i){
+            System.out.println(i);
+        }
+    }
+
+           
+   }
+}
