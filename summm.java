6e964c6443864e1117d49eb42e78b3ae48ff6b6d
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 21:42:32 +0530

    Create summm.java

---
+++ summm.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class summm{
+      public static void main(String aa[]){
+            int n;
+            int a;
+            int sum=0;
+            Scanner s=new Scanner(System.in);
+            a=s.nextInt();
+            n=s.nextInt();
+            int b[]=new int[n];
+            for(int i=0;i<n;i++){
+                b[i]=s.nextInt();
+            }
+            for(int i=0;i<=a;i++){
+             sum=sum+b[i];
+            }
+             System.out.println(sum);
+    }
+}
