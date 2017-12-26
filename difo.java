712e6b9bde783b0b448fd880d265323c737b7167
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 07:21:57 +0530

    Create difo.java

---
+++ difo.java
@@ -0,0 +1,21 @@
+  import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class difo{
+     public static void main(String aa[]){
+     int a;
+     int b;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     b=s.nextInt();
+     int c=a-b;
+     if(c%2==0){
+         System.out.println("even");
+     }
+ else{
+           System.out.println("odd");
+ }
+     
+   
+}
+    }
