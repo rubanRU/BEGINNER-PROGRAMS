0d0de9bfc1d19b088ececb3eb9ae0a5bdc476b9d
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 22:31:53 +0530

    Create mod.java

---
+++ mod.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class mod{
+
+     public static void main(String aa[]){
+    int a,b,c;
+    int h1;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+   b=s.nextInt();
+   c=s.nextInt();
+   h1=(a*b)%c;
+   System.out.println(h1);
+   
+           
+   }
+}
+
