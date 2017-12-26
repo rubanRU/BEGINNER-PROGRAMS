42bd5707a9e5eb9323a9bd8eb3860338d8427e49
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 07:03:02 +0530

    Create mu.java

---
+++ mu.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class mul{
+     public static void main(String aa[]){
+     int a;
+     int n=10;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     for(int i=10;i<10000;i+=10){
+         if(a<i){
+             System.out.println(a);
+         }
+     }
+     
+   
+}
+    }
