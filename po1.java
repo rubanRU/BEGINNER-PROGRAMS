d8e28281b7901bc03833ef2b8fa5426c953a227b
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 18:44:05 +0530

    Rename po.java to po1.java

---
+++ po1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class po{
+     public static void main(String aa[]){
+       int b;
+        Scanner s=new Scanner(System.in);
+       b=s.nextInt();
+       if(b%13==0){
+           System.out.println("Yes");
+       }
+ else{
+           System.out.println("No");
+ }
+  
+}
+    }
