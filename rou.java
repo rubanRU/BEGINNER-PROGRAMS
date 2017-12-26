11ef391d90a78ada124619b20eb86bb58976d4dd
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 18:11:21 +0530

    Create rou.java

---
+++ rou.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class rou{
+     public static void main(String aa[]){
+        float d;
+        Scanner s=new Scanner(System.in);
+        d=s.nextFloat();
+      int v=  Math.round(d);
+      System.out.println(v);
+  
+}
+    }
