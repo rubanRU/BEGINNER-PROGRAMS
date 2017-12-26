8bfb9fed51107762b4467d72c8b95b86cd96a811
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 23:37:33 +0530

    Create doup.java

---
+++ doup.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class doup{
+    public static void main(String aa[]){
+    double a,b,c;
+   Scanner s=new Scanner(System.in);
+    a=s.nextDouble();
+    b=s.nextDouble();
+    c=Math.pow(a, b);
+    System.out.print(c);
+  
+   
+           
+   }
+}
+
