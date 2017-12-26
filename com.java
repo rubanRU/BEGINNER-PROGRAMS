f9d0f0717e60f70f91b3c79792c0647ad0e76dc1
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 22:15:19 +0530

    Create com.java

---
+++ com.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+public class com{
+      public static void main(String aa[]){
+           int n;
+           int f=0;
+           Scanner s=new Scanner(System.in);
+           n=s.nextInt();
+          for(int i=2;i<n;i++){
+              if(n%i==0){
+                  f=1;
+                  break;
+              }
+          }
+           if(f==1){
+               System.out.println("YES");
+           }
+         else{
+               System.out.println("NO");
+         }
+
+    }
