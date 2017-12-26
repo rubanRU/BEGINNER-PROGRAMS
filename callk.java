e3f7731d032f2e6df77255966055f3d81a76a1a0
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 21:53:32 +0530

    Create callk.java

---
+++ callk.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class callk{
+
+     public static void main(String aa[]){
+    int a,b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    b=s.nextInt();
+    c=s.nextInt();
+    int p=(a*b*c)/100;
+    System.out.println(p);
+
+           
+   }
+}
+
