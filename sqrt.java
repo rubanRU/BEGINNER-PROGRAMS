cc0d93bab2540738e34eb5a6a3c8a68849b185e4
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 18:50:35 +0530

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class sqrt{
+     public static void main(String aa[]){
+       int b;
+        Scanner s=new Scanner(System.in);
+       b=s.nextInt();
+      int c=s.nextInt();
+      int h=b*c;
+      double j=Math.sqrt(h);
+      if(j==c){
+          System.out.println("Yes");
+      }
+ else{
+          System.out.println("No");
+ }
+}
+    }
