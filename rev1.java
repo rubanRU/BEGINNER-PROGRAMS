820fe7d9d51a6927973c3f8d51c19cb2e9292102
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 22:22:31 +0530

    Create rev1.java

---
+++ rev1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class rev1 {
+
+     public static void main(String aa[]){
+    int a,b,c;
+    Scanner s=new Scanner(System.in);
+   a=s.nextInt();
+   String n=String.valueOf(a);
+   String z=new StringBuffer(n).reverse().toString();
+   System.out.println(z);
+
+           
+   }
+}
