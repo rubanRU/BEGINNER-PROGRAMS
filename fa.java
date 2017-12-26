c6e5587197a18794d332d5fbd89956189999e848
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-26 18:41:28 +0530

    Create fa.java

---
+++ fa.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class fa{
+     public static void main(String aa[]){
+       int b;
+        Scanner s=new Scanner(System.in);
+        b=s.nextInt();
+        for(int i=1;i<=b;i++){
+            if(b%i==0){
+                System.out.print(i);
+            }
+        }
+      
+  
+}
+    }
