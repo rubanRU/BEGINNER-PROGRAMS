3a5d5c2b463484a662bdb5360c7bc90dc8f9a5e3
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-27 06:52:15 +0530

    Create ca.java

---
+++ ca.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class ca{
+    public static void main(String aa[]){
+   
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+   String b=String.valueOf(a);
+   System.out.println(b.length());
+           
+   }
+}
+
