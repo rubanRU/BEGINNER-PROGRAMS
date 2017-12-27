438e2e125043f03a8f0f187845853b60ce85a697
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-27 06:42:20 +0530

    Create arthh.java

---
+++ arthh.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+public class arthh{
+      public static void main(String aa[]){
+      int n;
+      int f;
+      int l;
+      int sum=0;
+      int val;
+      Scanner s=new Scanner(System.in);
+      n=s.nextInt();
+      f=s.nextInt();
+      l=s.nextInt();
+      val=f;
+      for(int i=0;i<n;i++){
+          sum=sum+val;
+          val=val+l;
+      }
+   System.out.println(sum);
+
+      
+}
+}
