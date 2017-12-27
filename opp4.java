f5777958544fd70efb5ef08c0ed6d6bf60b9e7e2
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-27 06:39:23 +0530

    Create opp4.java

---
+++ opp4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class opp4{
+    public static void main(String aa[]){
+   
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    int c=s.nextInt();
+    int d=(a*b)/c;
+    System.out.println(d);
+           
+   }
+}
+
