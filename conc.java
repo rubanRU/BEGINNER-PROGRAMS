2f2b257359599f6f0b6af96620f53988618b4475
rubanRU <34646101+rubanRU@users.noreply.github.com>
2017-12-27 17:56:06 +0530

    Create conc.java

---
+++ conc.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+public class conc{
+    public static void main(String aa[]){
+   
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    String h=String.valueOf(a);
+    String k=String.valueOf(b);
+    String j=h.concat(k);
+    System.out.println(j);
+
+}
+}
+
